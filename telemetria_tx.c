635c77bacfa665b7241b44a1f8def1abb981b6ef
Gabriel <jgabriel.orozco@udea.edu.co>
2022-09-05 12:30:02 -0500

    Se añade el sensor de viento.

--- telemetria_tx.c
+++ telemetria_tx.c
@@ -2,16 +2,50 @@
 #include "pico/stdlib.h"
 #include "nrf24_driver.h"
 #include "mpu9250.h"
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
+#include "hardware/adc.h"
+#include "hardware/timer.h"
 
+#define PI  3.14159265
+
+//Variables de la IMU
 int16_t acceleration[3], gyro[3], gyroCal[3], eulerAngles[2], fullAngles[2], magnet[3];
 absolute_time_t timeOfLastCheck;
 
+//Variables velocidad del viento
+const uint interruptPin = 16;
+uint count_holes = 0;
+uint count_steps = 0;
+const uint holes = 72; //Agujeros del encoder
+repeating_timer_t timer;
+float radSeg = 0;
+float windSpeed = 0;
+float radio = 0.09;
+
+//Variables dirección del viento
+const int direccionPin = 28;
+uint16_t direccionAdc;
+float direccionGrados;
+
+//Factor de converion del adc
+const float conversion_factor = 3.3f / (1 << 12);
+
+//Funciones sensor de viento
+void wind_init(void);
+void addHole();
+void getSpeed();
+bool everySecond();
+void setDireccion();
+
+//Funciones de la IMU
 void init_mpu9250(int loop);
 void updateAngles();
 void printDataImu();
 
 char str[100];
 
+//Direcciones de los pipes RF
 const uint8_t pipezero_addr[5] = {0x37,0x37,0x37,0x37,0x37};
 const uint8_t pipeone_addr[5] = {0xC7,0xC7,0xC7,0xC7,0xC7};
 const uint8_t pipetwo_addr[5] = {0xC8,0xC7,0xC7,0xC7,0xC7};
@@ -20,6 +54,8 @@ int main()
 {
     stdio_init_all();
     sleep_ms(3000);
+
+    wind_init();
 
     pin_manager_t pins_rf = { 
         .copi = 3,
@@ -47,7 +83,7 @@ int main()
     };
 
     // SPI baudrate
-     uint32_t my_baudrate = 5000000;
+    uint32_t my_baudrate = 5000000;
 
     nrf_client_t my_nrf;
 
@@ -83,8 +119,8 @@ int main()
 
     // payload sent to receiver data pipe 1
     typedef struct payload_one_s {
-        int16_t windSpeed;
-        int16_t windDir; 
+        int16_t windDir;
+        int16_t windSpeed; 
     } payload_one_t;
 
     typedef struct payload_two_s { int16_t angle1; int16_t angle2; } payload_two_t;
@@ -100,6 +136,7 @@ int main()
     while(1){
         updateAngles();
         printDataImu();
+        setDireccion();
 
         payload_zero_t payload_zero = {
             .acelX = acceleration[0],
@@ -114,12 +151,13 @@ int main()
         };
 
         payload_one_t payload_one = {
-            .windSpeed = 0x0F,
-            .windDir = 0x0E
+            .windDir = direccionGrados,
+            .windSpeed = windSpeed 
+           
         };
 
         // payload sent to receiver data pipe 2
-        payload_two_t payload_two = { .angle1 = fullAngles[0], .angle2 = fullAngles[1] };
+        payload_two_t payload_two = { .angle1= fullAngles[0], .angle2 = fullAngles[1]};
 
         //send to receiver's DATA_PIPE_0 address
         my_nrf.tx_destination(pipezero_addr);
@@ -144,7 +182,7 @@ int main()
         printf("\nPacket not sent:- Receiver not available.\n");
         }
 
-        sleep_ms(100);
+        sleep_ms(30);
 
         // send to receiver's DATA_PIPE_1 address
         my_nrf.tx_destination(pipeone_addr);
@@ -167,7 +205,7 @@ int main()
         printf("\nPacket not sent:- Receiver not available.\n");
         }
 
-        sleep_ms(100);
+        sleep_ms(30);
 
         // send to receiver's DATA_PIPE_2 address
         my_nrf.tx_destination(pipetwo_addr);
@@ -190,7 +228,7 @@ int main()
         printf("\nPacket not sent:- Receiver not available.\n");
         }
 
-        sleep_ms(100);
+        sleep_ms(30);
     }
 }
 
@@ -220,4 +258,45 @@ void printDataImu(){
     printf("%d,%d,%d\n", magnet[0], magnet[1], magnet[2]);
     //printf("Euler. Roll = %d, Pitch = %d\n", eulerAngles[0], eulerAngles[1]);
     //printf( "%d,%d\n", fullAngles[0], fullAngles[1]);
+}
+
+void wind_init(){
+    adc_init();
+
+    //GPIOs
+    gpio_init(interruptPin);
+
+    //IRQs
+    gpio_set_irq_enabled_with_callback(interruptPin, GPIO_IRQ_EDGE_RISE, true, &addHole);
+
+    //Alarmas
+    add_repeating_timer_ms(1000, everySecond, NULL, &timer);
+
+    //Dirección
+    adc_gpio_init(direccionPin);
+    adc_select_input(2);
+
+    sleep_ms(10);
+}
+
+void addHole(){    
+    count_holes += 1;
+}
+
+bool everySecond(){
+    getSpeed();
+}
+
+void getSpeed(){
+    radSeg = ((count_holes * 60) / holes)*2*PI/60;
+    windSpeed = radSeg * radio;
+    printf("Velocidad: %f \n", windSpeed);
+    count_holes = 0;
+}
+
+void setDireccion(){
+    direccionAdc = adc_read();
+    // printf("Raw value: 0x%03x, voltage: %f V\n", direccionAdc, direccionAdc * conversion_factor);
+    direccionGrados = (direccionAdc * conversion_factor)*(359.0/3.3);
+    printf("Direccion: %f °\n", direccionGrados);
 }
